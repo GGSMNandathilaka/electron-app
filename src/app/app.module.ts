cf0903fffc988369b6060f84cdb863cbdae92af3
GGSMNandathilaka <ggsmnandathilaka>
2017-12-26 22:54:12 +0530

    Implement timer app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {RoundProgressModule} from "angular-svg-round-progressbar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
